dee262616b6b12f24493d43b4988b450efbf99aa
Andre Wells <Andre.Wells@korbitecinc.com>
2021-10-17 19:52:06 +0200

    Added initial classes

---
+++ MediatRUseCaseItemTemplate/UseCaseHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatRUseCaseItemTemplate
+{
+    public class UseCaseHandler : IRequestHandler<UseCaseRequest, UseCaseResponse>
+    {
+        private readonly ILogger<UseCaseHandler> _logger;
+
+        public UseCaseHandler(ILogger<UseCaseHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<UseCaseResponse> Handle(UseCaseRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
